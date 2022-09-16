066a70ae4b978e8a9800dc50b5a353a1473704f4
KangJaeHoon <morateng@naver.com>
2022-09-16 22:28:32 +0900

    week2

--- week2/hello.R
+++ week2/hello.R
@@ -10,3 +10,20 @@ print(nanugi)
 print(mok)
 print(nameoji)
 print(jegop)
+
+25+99
+456-123
+2*(3+4)
+(3+5*6)/7
+
+a <- 'd'
+
+
+a <- 4
+b <- 5
+c <- a+b
+print(c)
+
+x <- c(6, 8, 10)    # c()는 벡터
+y <- 2*x*x+5*x+10
+print(y)
